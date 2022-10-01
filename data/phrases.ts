f81041a8d69eecf54ae5b70b8a53250327274168
jintak0401 <jintak0401@naver.com>
2022-10-01 12:04:50 +0900

    feat: 라우팅, 문구, 프로젝트 데이터, 메타데이터 추가

---
+++ data/phrases.ts
@@ -0,0 +1,59 @@
+const phrases = {
+  latestPosts: '최신 포스트',
+  search: '검색',
+  noPostsFound: '포스트를 찾을 수 없어요...',
+  blogDesc: 'Time Gambit의 블로그',
+  prev: '이전 포스트',
+  next: '다음 포스트',
+  editOnGithub: 'GitHub에서 보기',
+  allPosts: '모든 포스트',
+  visit: '방문하기',
+  comments: '댓글수',
+  views: '조회수',
+  delete: '삭제',
+  cancel: '취소',
+  toc: 'Table of contents',
+  delete_successful: '성공적으로 삭제했습니다',
+  SignIn: {
+    title: 'Time Gambit 블로그에 어서오세요! 다음으로 로그인 해주세요.',
+  },
+  Tooltip: {
+    darkmode: '다크모드',
+    lightmode: '라이트모드',
+    search: '검색',
+  },
+  Seo: {
+    defaultDesc: 'Time Gambit의 블로그',
+    dashboardDesc: '대시보드',
+    projectsDesc: '개발해온 프로젝트',
+    guestbookDesc: '게스트북',
+  },
+  Guestbook: {
+    description:
+      '아무 이야기나 써주세요! 블로그 피드백이나 잡담도 상관없습니다',
+    success: '로그인에 성공했어요!',
+    error: '메시지를 입력해주세요',
+    guestbook: 'Guestbook',
+    signInGuestbook: 'Guestbook 로그인',
+    signIn: '로그인',
+    placeholder: '여기에 메시지를 입력해주세요',
+    message: '메시지',
+    sign: '메시지 등록',
+    tip: '아바타랑 이름만 보여집니다',
+    signOut: '로그아웃',
+    deleteModalTitle: '메시지 삭제',
+  },
+  _404: {
+    title: '잘못된 주소입니다!',
+    description:
+      '이 페이지는 삭제 혹은 다른 주소로 변경되었거나, 잘못된 주소입니다.',
+    button: '홈으로 갈게요!',
+  },
+  _500: {
+    title: '서버가 이상하네요...',
+    description: '서버가 요청을 받지 못하고 있어요... 곧 해결할게요!',
+    button: '새로고침',
+  },
+};
+
+export default phrases;
---
+++ data/siteMetadata.ts
@@ -0,0 +1,53 @@
+const siteMetadata = {
+  title: 'Time Gambit Blog',
+  author: 'Time Gambit | 한진탁',
+  headerTitle: 'Time Gambit',
+  description: 'Time Gambit의 블로그',
+  language: 'ko-KR',
+  theme: 'system', // system, dark or light
+  siteUrl: 'http://localhost:3000',
+  siteRepo: 'https://github.com/jintak/timegambit-blog',
+  siteLogo: '/static/images/logo.png',
+  image: '/static/images/avatar.png',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'jintak0401@naver.com',
+  github: 'https://github.com',
+  twitter: 'https://twitter.com/Twitter',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
+  locale: 'ko-KR',
+  analytics: {
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  },
+  comment: {
+    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    giscusConfig: {
+      // Visit the link below, and follow the steps in the 'configuration' section
+      // https://giscus.app/
+      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+      repoId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      mapping: 'pathname',
+      reactionsEnabled: '1',
+      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
+      emitMetaData: '0',
+      // theme example: light, dark, dark_dimmed, dark_high_contrast
+      // transparent_dark, preferred_color_scheme, custom
+      theme: 'light',
+      // Place the comment box above the comments. options: bottom, top
+      inputPosition: 'bottom',
+      // Choose the language giscus will be displayed in. options: en, es, zh-CN, zh-TW, ko, ja etc
+      lang: 'ko',
+      // theme when dark mode
+      darkTheme: 'transparent_dark',
+      // If the theme option above is set to 'custom`
+      // please provide a link below to your custom theme css file.
+      // example: https://giscus.app/themes/custom_example.css
+      themeURL: '',
+    },
+  },
+};
+
+export default siteMetadata;
