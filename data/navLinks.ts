e9a5e2c6ed7df15e5ff9b47e364f85a7fefe6e37
jintak0401 <32933980+jintak0401@users.noreply.github.com>
2022-10-01 12:08:43 +0900

    Merge pull request #3
    * chore: 임시 이미지 추가
    
    * chore: 절대경로 추가
    
    * chore: next-themes 추가
    
    * chore: next-themes 추가
    
    * chore: line-break 에러방지
    
    * feat: 라우팅, 문구, 프로젝트 데이터, 메타데이터 추가

---
+++ data/navLinks.ts
@@ -0,0 +1,9 @@
+const navLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/categories', title: 'Categories' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+];
+
+export default navLinks;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import siteMetadata from '@data/siteMetadata';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
