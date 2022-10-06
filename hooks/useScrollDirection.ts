626bab1b4dde2e418dbf154dddae3fc3d3e48f45
jintak0401 <jintak0401@naver.com>
2022-10-06 18:16:01 +0900

    refactor: useScrollDirection 리팩토링
    - useEffect는 클라이언트에서 수행되므로 isClient 변수 제거

--- hooks/useScrollDirection.ts
+++ hooks/useScrollDirection.ts
@@ -3,19 +3,14 @@ import { useEffect, useRef, useState } from 'react';
 import throttle from '@/lib/throttle';
 
 export const useScrollDirection = () => {
-  const isClient = typeof window === 'object';
-  const getScrollYPos = () => (isClient ? window.scrollY : undefined);
-
   const scrollYPos = useRef(0);
   const [scrollDirection, setScrollDirection] = useState('up');
 
   useEffect(() => {
-    if (!isClient) return;
-
     const handleScroll = () => {
-      const newY = getScrollYPos() as number;
+      const newY = window.scrollY;
       setScrollDirection(scrollYPos.current > newY ? 'up' : 'down');
-      scrollYPos.current = getScrollYPos() as number;
+      scrollYPos.current = window.scrollY;
     };
 
     const throttleHandleScroll = throttle(handleScroll, 100);
