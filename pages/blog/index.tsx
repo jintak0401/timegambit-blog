3ab421d158664f147205913169c66f611baed647
jintak0401 <jintak0401@naver.com>
2022-10-12 23:34:36 +0900

    feat: Blog 페이지 구현

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,3 +1,47 @@
-export default function BlogPage() {
-  return <div>블로그</div>;
+import { allBlogs } from 'contentlayer/generated';
+import { InferGetStaticPropsType } from 'next';
+
+import { pick, sortedBlogPost } from '@/lib/contentlayer';
+
+import phrases from '@/data/phrases';
+import siteMetadata from '@/data/siteMetadata';
+
+import { PageSEO } from '@/components/SEO';
+
+import ListLayout from '@/layouts/ListLayout';
+
+export const getStaticProps = async () => {
+  const posts = sortedBlogPost(allBlogs)
+    .filter(({ draft }) => !draft)
+    .map((post) =>
+      pick(post, ['title', 'slug', 'date', 'tags', 'summary', 'images'])
+    );
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default function BlogPage({
+  posts,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <>
+      <PageSEO
+        title={`Blog - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+      />
+      <ListLayout
+        posts={posts}
+        /*
+        initialDisplayPosts={initialDisplayPosts}
+        pagination={pagination}
+*/
+        title={phrases.Blog.title}
+        description={phrases.Blog.description}
+      />
+    </>
+  );
 }
