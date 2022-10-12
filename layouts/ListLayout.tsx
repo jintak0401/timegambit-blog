22d19f076c65d38fad004476969ff970b604fca4
jintak0401 <jintak0401@naver.com>
2022-10-12 23:34:23 +0900

    feat: Blog 페이지의 ListLayout 수정

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -1,52 +1,43 @@
-import type { Blog } from 'contentlayer/generated';
-import { ComponentProps, useState } from 'react';
+import { useState } from 'react';
 
-import { CoreContent } from '@/lib/contentlayer';
-import formatDate from '@/lib/formatDate';
+import { PostListItem } from '@/lib/types';
 
-import Link from '@/components/Link';
-import Pagination from '@/components/Pagination';
-import Tag from '@/components/Tag';
+import phrases from '@/data/phrases';
+
+import PostCard from '@/components/PostCard';
 
 interface Props {
-  posts: CoreContent<Blog>[];
+  posts: PostListItem[];
   title: string;
-  initialDisplayPosts?: CoreContent<Blog>[];
-  pagination?: ComponentProps<typeof Pagination>;
+  description?: string;
 }
 
-export default function ListLayout({
-  posts,
-  title,
-  initialDisplayPosts = [],
-  pagination,
-}: Props) {
+export default function ListLayout({ posts, title, description }: Props) {
   const [searchValue, setSearchValue] = useState('');
-  const filteredBlogPosts = posts.filter((post) => {
+  const filteredBlogPosts = posts.filter((post: PostListItem) => {
     const searchContent = post.title + post.summary + post.tags?.join(' ');
     return searchContent.toLowerCase().includes(searchValue.toLowerCase());
   });
 
   // If initialDisplayPosts exist, display it if no searchValue is specified
   const displayPosts =
-    initialDisplayPosts.length > 0 && !searchValue
-      ? initialDisplayPosts
-      : filteredBlogPosts;
+    posts.length > 0 && !searchValue ? posts : filteredBlogPosts;
 
   return (
     <>
       <div className="divide-y">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-3xl font-extrabold leading-9 text-gray-900 transition-colors duration-500 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
+          {description && <p className="text-gray-500">{description}</p>}
           <div className="relative max-w-lg">
             <input
               aria-label="Search articles"
               type="text"
               onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Search articles"
-              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 transition-colors duration-500 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
             />
             <svg
               className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
@@ -65,50 +56,14 @@ export default function ListLayout({
           </div>
         </div>
         <ul>
-          {!filteredBlogPosts.length && 'No posts found.'}
-          {displayPosts.map((post) => {
-            const { slug, date, title, summary, tags } = post;
-            return (
-              <li key={slug} className="py-4">
-                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                  <dl>
-                    <dt className="sr-only">Published on</dt>
-                    <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                      <time dateTime={date}>{formatDate(date)}</time>
-                    </dd>
-                  </dl>
-                  <div className="space-y-3 xl:col-span-3">
-                    <div>
-                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-gray-900 dark:text-gray-100"
-                        >
-                          {title}
-                        </Link>
-                      </h3>
-                      <div className="flex flex-wrap">
-                        {tags?.map((tag) => (
-                          <Tag key={tag} text={tag} />
-                        ))}
-                      </div>
-                    </div>
-                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                      {summary}
-                    </div>
-                  </div>
-                </article>
-              </li>
-            );
-          })}
+          {!filteredBlogPosts.length && phrases.Blog.noPost}
+          {displayPosts.map((post) => (
+            <li key={post.title}>
+              <PostCard post={post} />
+            </li>
+          ))}
         </ul>
       </div>
-      {pagination && pagination.totalPages > 1 && !searchValue && (
-        <Pagination
-          currentPage={pagination.currentPage}
-          totalPages={pagination.totalPages}
-        />
-      )}
     </>
   );
 }
