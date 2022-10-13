767c8f24192bc05a4db4e3d29c2d04406d33b0ef
jintak0401 <jintak0401@naver.com>
2022-10-14 00:03:47 +0900

    feat: 필요한 요소만 사용하도록 코드 수정 및 텍스트 재배치

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,35 +2,64 @@ import siteMetadata from '@/data/siteMetadata';
 
 import SocialIcon from '@/components/social-icons';
 
-import Link from './Link';
+const ICON_SIZE = 5;
 
 export const Footer = () => {
   return (
     <footer>
-      <div className="mt-16 flex flex-col items-center">
+      <div className="my-16 flex flex-col items-center">
+        <div className="mb-4 text-lg text-gray-500 dark:text-gray-400">
+          {siteMetadata.description}
+        </div>
         <div className="mb-3 flex space-x-4">
-          <SocialIcon
-            kind="mail"
-            href={`mailto:${siteMetadata.email}`}
-            size={6}
-          />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
+          {siteMetadata.email && (
+            <SocialIcon
+              kind="mail"
+              href={`mailto:${siteMetadata.email}`}
+              size={ICON_SIZE}
+            />
+          )}
+          {siteMetadata.github && (
+            <SocialIcon
+              kind="github"
+              href={siteMetadata.github}
+              size={ICON_SIZE}
+            />
+          )}
+          {siteMetadata.facebook && (
+            <SocialIcon
+              kind="facebook"
+              href={siteMetadata.facebook}
+              size={ICON_SIZE}
+            />
+          )}
+          {siteMetadata.youtube && (
+            <SocialIcon
+              kind="youtube"
+              href={siteMetadata.youtube}
+              size={ICON_SIZE}
+            />
+          )}
+          {siteMetadata.linkedin && (
+            <SocialIcon
+              kind="linkedin"
+              href={siteMetadata.linkedin}
+              size={ICON_SIZE}
+            />
+          )}
+          {siteMetadata.twitter && (
+            <SocialIcon
+              kind="twitter"
+              href={siteMetadata.twitter}
+              size={ICON_SIZE}
+            />
+          )}
+          <SocialIcon kind="rss" size={ICON_SIZE} href="/feed.xml" />
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
-        </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
-          </Link>
+          <div>•</div>
+          <div>{siteMetadata.author}</div>
         </div>
       </div>
     </footer>
