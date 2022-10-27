8278c13bffcd06da810002ec1cb00433aa770acd
jintak0401 <jintak0401@naver.com>
2022-10-28 00:08:36 +0900

    feat: about 페이지에 보여질 데이터

---
+++ data/about.ts
@@ -0,0 +1,44 @@
+const about = {
+  Contact: {
+    email: 'jintak0401@naver.com',
+    github: 'https://github.com/jintak0401',
+    blog: 'https://timegambit.com',
+  },
+  Skills: [
+    {
+      title: 'FrontEnd',
+      info: ['Next JS', 'React', 'TypeScript', 'JavaScript', 'HTML/CSS'],
+    },
+    { title: 'BackEnd', info: ['Express', 'Nest JS', 'MySQL', 'AWS'] },
+  ],
+  Experience: [
+    {
+      title: '우아한 테크캠프',
+      date: '2022.07.04 ~ 2022.08.31',
+      info: [
+        '8주 동안 5개의 프로젝트 진행',
+        '웹 프론트엔드 중심으로 백엔드를 함께 배우는 자기 주도형 풀스택 과정',
+        '페어 프로그래밍, 팀 프로젝트를 통한 협업 경험',
+        'Vanilla JS, TypeScript, React, Express, Nest JS, AWS 외 다수',
+      ],
+    },
+    {
+      title: 'SW 마에스트로 11기',
+      date: '2020.06.01 ~ 2020.11.30',
+      info: [
+        '과학기술정보통신부에서 주관하는 SW개발자 양성 프로그램',
+        '7개월간 개인 맞춤형 문제 제공 프로젝트 진행',
+        'Expo, React Native',
+      ],
+    },
+  ],
+  Education: [
+    {
+      title: '한양대학교',
+      date: '2016.03 ~ 현재',
+      info: ['소프트웨어 전공', '학점 4.24 / 4.5'],
+    },
+  ],
+};
+
+export default about;
