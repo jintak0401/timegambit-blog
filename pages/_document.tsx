d423581cd2706550728ca86898155878c2e09f00
jintak0401 <jintak0401@naver.com>
2022-10-28 21:53:57 +0900

    feat: favicon 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,30 +6,94 @@ class MyDocument extends Document {
       <Html lang="ko" className="scroll-smooth">
         <Head>
           <link
-            rel="apple-touch-icon"
+            rel="apple-touch-icon-precomposed"
+            sizes="57x57"
+            href="/static/favicons/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="114x114"
+            href="/static/favicons/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="72x72"
+            href="/static/favicons/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href="/static/favicons/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="60x60"
+            href="/static/favicons/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="120x120"
+            href="/static/favicons/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
             sizes="76x76"
-            href="/static/favicons/apple-touch-icon.png"
+            href="/static/favicons/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="152x152"
+            href="/static/favicons/apple-touch-icon-152x152.png"
           />
           <link
             rel="icon"
             type="image/png"
-            sizes="32x32"
+            href="/static/favicons/favicon-196x196.png"
+            sizes="196x196"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/static/favicons/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/png"
             href="/static/favicons/favicon-32x32.png"
+            sizes="32x32"
           />
           <link
             rel="icon"
             type="image/png"
-            sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
+            sizes="16x16"
           />
-          <link rel="manifest" href="/static/favicons/site.webmanifest" />
           <link
-            rel="mask-icon"
-            href="/static/favicons/safari-pinned-tab.svg"
-            color="#5bbad5"
+            rel="icon"
+            type="image/png"
+            href="/static/favicons/favicon-128.png"
+            sizes="128x128"
           />
-          <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="application-name" content="&nbsp;" />
+          <meta name="msapplication-TileColor" content="#FFFFFF" />
+          <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+          <meta
+            name="msapplication-square70x70logo"
+            content="mstile-70x70.png"
+          />
+          <meta
+            name="msapplication-square150x150logo"
+            content="mstile-150x150.png"
+          />
+          <meta
+            name="msapplication-wide310x150logo"
+            content="mstile-310x150.png"
+          />
+          <meta
+            name="msapplication-square310x310logo"
+            content="mstile-310x310.png"
+          />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
         </Head>
         <body className="antialiased">
