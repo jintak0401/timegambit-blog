0efd5a9e368e8e3629eda55ce56b063258694d25
jintak0401 <jintak0401@naver.com>
2022-10-29 10:07:45 +0900

    feat: About 페이지 스타일 수정
    - 화면크기별 margin, font 사이즈 다르게 수정
    - 폰트 색상 및 transition 추가

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,5 @@
 import about from '@/data/about';
+import phrases from '@/data/phrases';
 import siteMetadata from '@/data/siteMetadata';
 
 import AboutList from '@/components/AboutList';
@@ -9,13 +10,18 @@ import { PageSEO } from '@/components/SEO';
 const SELFIE_URL = '/static/images/selfie.jpg';
 
 export default function AboutPage() {
+  const { title, description } = phrases.About;
   return (
     <>
       <PageSEO
         title={`About - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
-      <section className="mb-20 flex flex-col space-x-0 space-y-5 md:flex-row md:space-y-0 md:space-x-7">
+      <h1 className="basic-text text-3xl font-extrabold md:text-5xl">
+        {title}
+      </h1>
+      {description && <p className="text-gray-500">{description}</p>}
+      <section className="mb-7 mt-12 flex flex-col space-x-0 space-y-5 md:mt-20 md:mb-10 md:flex-row md:space-y-0 md:space-x-7">
         <Image
           alt="셀카"
           src={SELFIE_URL}
@@ -26,14 +32,18 @@ export default function AboutPage() {
           layout="fixed"
         />
         <div className="flex flex-col justify-center">
-          <h2 className="mb-4 text-4xl font-bold">Contact & Channel</h2>
+          <h2 className="basic-text mb-4 text-2xl font-bold md:text-4xl">
+            Contact & Channel
+          </h2>
           <ul
             role="list"
             className="list-inside list-disc space-y-3 text-lg marker:text-primary-500"
           >
             {Object.entries(about.Contact).map(([key, value]) => (
               <li role="listitem" key={key}>
-                <span className="inline-block w-[72px] md:w-20">{key}.</span>
+                <span className="basic-text -ml-1.5 inline-block w-[72px] md:w-20">
+                  {key}.
+                </span>
                 <Link
                   className="font-medium text-primary-500 hover:text-primary-600 hover:underline dark:hover:text-primary-400"
                   href={`${value.includes('@') ? 'mailto:' : ''}${value}`}
