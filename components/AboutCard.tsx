c6145eece3124dd90a3cfcb6a50baf8f7248c017
jintak0401 <jintak0401@naver.com>
2022-10-29 10:28:53 +0900

    feat: About 페이지 스타일 수정
    - 화면크기별 margin, font 사이즈 다르게 수정
    - 폰트 색상 및 transition 추가

--- components/AboutCard.tsx
+++ components/AboutCard.tsx
@@ -6,18 +6,20 @@ interface Props {
 
 const AboutCard = ({ title, date, info }: Props) => {
   return (
-    <article className="flex flex-col space-y-4 lg:flex-row lg:space-y-0">
-      <div className="flex flex-col space-y-3 lg:w-80">
-        <h3 className="text-3xl font-semibold">{title}</h3>
-        {date && <i>{date}</i>}
+    <article className="flex flex-col space-y-4 md:flex-row md:space-y-0">
+      <div className="text- flex w-72 flex-col md:space-y-3">
+        <h3 className="basic-text text-xl font-semibold md:text-3xl">
+          {title}
+        </h3>
+        {date && <i className="text-gray-400 dark:text-gray-500">{date}</i>}
       </div>
       <ul
         role="list"
-        className="list-inside list-disc space-y-3 marker:text-primary-500 lg:list-outside"
+        className="basic-text list-inside list-disc space-y-3 marker:text-primary-500"
       >
         {info.map((piece) => (
           <li role="listitem" key={piece}>
-            {piece}
+            <span className="-ml-1.5">{piece}</span>
           </li>
         ))}
       </ul>
