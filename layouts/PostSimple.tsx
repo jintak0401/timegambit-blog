e9f6dba02f92f71dc4e489d2571ea231577a7f9a
jintak0401 <jintak0401@naver.com>
2022-11-04 12:28:04 +0900

    feat: 본문의 divide-y 제거

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -51,7 +51,7 @@ export default function PostLayout({ content, next, prev, children }: Props) {
             className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <div className="relative divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="relative divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">
                 {children}
               </div>
