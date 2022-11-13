0f75ae9a58a8863754326e40f2c2e0f25a4faf59
jintak0401 <jintak0401@naver.com>
2022-11-14 00:29:55 +0900

    Revert "feat: 선택된 태그 글 목록 페이지 구현 (#90)"
    This reverts commit 468c4c016c57b4f3f5cc7863befee81554defe69.

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Tag = ({ text }: Props) => {
   return (
     <Link href={`/tags/${slugger.slug(text)}`}>
-      <a className="mr-3 text-sm font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
         {text.split(' ').join('-')}
       </a>
     </Link>
--- pages/series/index.tsx
+++ pages/series/index.tsx
@@ -1,10 +1,3 @@
 export default function SeriesPage() {
-  return (
-    <div className="flex h-40 items-center justify-center">
-      <h1 className="text-center text-xl font-bold sm:text-3xl">
-        시리즈를 모아놓을 페이지입니다.
-        <br />곧 완성할 예정입니다!
-      </h1>
-    </div>
-  );
+  return <div>시리즈</div>;
 }
