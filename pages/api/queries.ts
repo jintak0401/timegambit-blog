fecab37c62a1d6a92cfa52e45d85f5634d8dcbe4
jintak0401 <jintak0401@naver.com>
2022-11-24 14:07:52 +0900

    feat: DELETE_GUESTBOOK 쿼리에 id와 email이 일치하는 방명록 삭제하도록 쿼리문 수정

--- pages/api/queries.ts
+++ pages/api/queries.ts
@@ -9,14 +9,14 @@ const queries = {
   READ_VIEW_COUNT: 'SELECT VIEW_COUNT as count FROM VIEW_TB WHERE slug=?',
   READ_ALL_VIEW_COUNT: 'SELECT SUM(VIEW_COUNT) as count FROM VIEW_TB',
   READ_ALL_GUESTBOOK:
-    'SELECT id, email, body, name, image FROM GUESTBOOK_TB ORDER BY updated_at DESC',
+    'SELECT id, email, body, name, image, DATE_FORMAT(updated_at, "%Y년 %m월 %d일") AS updatedAt FROM GUESTBOOK_TB ORDER BY updated_at DESC',
   // [email, name, image, body]
   CREATE_GUESTBOOK:
     'INSERT INTO GUESTBOOK_TB (email, name, image, body) VALUES (?, ?, ?, ?)',
   // [body, id]
   UPDATE_GUESTBOOK: 'UPDATE GUESTBOOK_TB SET body=? WHERE id=?',
-  // [id]
-  DELETE_GUESTBOOK: 'DELETE FROM GUESTBOOK_TB WHERE id=?',
+  // [id, email]
+  DELETE_GUESTBOOK: 'DELETE FROM GUESTBOOK_TB WHERE id=? AND email=?',
 };
 
 export default queries;
