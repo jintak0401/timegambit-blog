cbb7a41e4e9e6bae2355235cb75a37abb89091a5
jintak0401 <jintak0401@naver.com>
2022-11-24 16:39:07 +0900

    feat: 로딩시 Spinner를 보여주도록 추가 구현

--- components/Guestbook/Guestbook.tsx
+++ components/Guestbook/Guestbook.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useSession } from 'next-auth/react';
 
 import { UserType } from '@/lib/types';
@@ -6,21 +7,26 @@ import phrases from '@/data/phrases';
 
 import GuestbookInput from '@/components/Guestbook/GuestbookInput';
 import GuestbookLogin from '@/components/Guestbook/GuestbookLogin';
+import Spinner from "@/components/Spinner";
 
 const Guestbook = () => {
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="strong-text space-y-6 rounded-md border-2 p-4 md:p-10">
-      <h2 className="text-2xl font-semibold md:text-3xl">
-        {session
-          ? phrases.Guestbook.messageGuestbook
-          : phrases.Guestbook.loginGuestbook}
-      </h2>
+      <header className="flex gap-4 justify-start items-center">
+        <h2 className="text-2xl font-semibold md:text-3xl">
+          {session
+            ? phrases.Guestbook.messageGuestbook
+            : phrases.Guestbook.loginGuestbook}
+        </h2>
+        {loading && <Spinner />}
+      </header>
       {session?.user ? (
-        <GuestbookInput user={session.user as UserType} />
+        <GuestbookInput user={session.user as UserType} setLoading={setLoading} />
       ) : (
-        <GuestbookLogin />
+        <GuestbookLogin setLoading={setLoading} />
       )}
     </div>
   );
