ce9a3e492bb4ecacd8aea223609dcc2f5a3cebad
jintak0401 <jintak0401@naver.com>
2022-11-30 22:03:02 +0900

    fix: src와 alt를 nullable로 변경 & null일 경우 대체이미지 보여주도록 수정

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,11 +1,13 @@
 import AutoImage from '@/components/AutoImage';
 
 interface Props {
-  src: string;
-  alt: string;
+  src?: string;
+  alt?: string;
 }
 
 const Image = ({ src, alt }: Props) => {
+  src = src || 'https://i.imgur.com/EJwWeUH.png';
+  alt = alt || '이미지를 불러올 수 없습니다';
   return (
     <div className="mb-6 space-y-2 md:space-y-3">
       <AutoImage loading="lazy" src={src} alt={alt} />
