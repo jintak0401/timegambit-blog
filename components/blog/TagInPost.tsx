0e265d8112b1d91190aa6fd666b8a1a43222c90d
jintak0401 <32933980+jintak0401@users.noreply.github.com>
2022-12-03 14:30:21 +0900

    TagInPost 컴포넌트 구현 (#159)
    * feat: sm 사이즈일 때 padding 제거
    
    * fix: 감싸고 있던 SectionContainer 제거
    
    * feat: Link 태그 내 div 태그를 a 태그로 수정
    
    * feat: TagInPost 컴포넌트 구현
    
    * feat: TagInPost 컴포넌트 적용
    
    * refactor: TagInPost 컴포넌트를 /components/blog로 이동

---
+++ components/blog/TagInPost.tsx
@@ -0,0 +1,27 @@
+import { slug } from 'github-slugger';
+import Link from 'next/link';
+import { AiFillTag } from 'react-icons/ai';
+
+interface Props {
+  title: string;
+}
+
+const TagInPost = ({ title }: Props) => {
+  return (
+    <Link href={`/tags/${slug(title)}`}>
+      <a
+        className="group flex items-center gap-1.5 rounded-md bg-gray-200 py-1 px-2
+        text-sm text-gray-600 duration-200 hover:bg-gray-600 hover:text-gray-200
+        dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+      >
+        <AiFillTag
+          className="h-[14px] w-[14px] duration-200 group-hover:text-gray-200
+         dark:text-gray-200"
+        />
+        {title}
+      </a>
+    </Link>
+  );
+};
+
+export default TagInPost;
--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -11,6 +11,7 @@ import PageTitle from '@/components/blog/PageTitle';
 import PostListInSeries from '@/components/blog/PostListInSeries';
 import ScrollIndicator from '@/components/blog/ScrollIndicator';
 import ScrollTopAndComment from '@/components/blog/ScrollTopAndComment';
+import TagInPost from '@/components/blog/TagInPost';
 import ViewCounter from '@/components/blog/ViewCounter';
 import Comments from '@/components/comments';
 import { BlogSEO } from '@/components/common/SEO';
@@ -35,7 +36,7 @@ export default function PostLayout({
   seriesTitle,
   children,
 }: Props) {
-  const { slug, date, title } = content;
+  const { slug, date, title, tags } = content;
 
   return (
     <>
@@ -63,18 +64,23 @@ export default function PostLayout({
             </div>
           </header>
           <div
-            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 "
+            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0"
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            <div className="relative divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+            <div className="relative divide-gray-200 pb-5 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               {seriesTitle && series && (
                 <PostListInSeries seriesTitle={seriesTitle} series={series} />
               )}
-              <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">
+              <div className="prose max-w-none pb-10 pt-10 dark:prose-dark sm:pb-12 xl:pb-20">
                 {children}
               </div>
               <LargeWidthTOC />
               <SmallWidthTOC />
+              <div className="flex gap-3">
+                {tags?.map((tag) => (
+                  <TagInPost key={tag} title={tag} />
+                ))}
+              </div>
             </div>
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
