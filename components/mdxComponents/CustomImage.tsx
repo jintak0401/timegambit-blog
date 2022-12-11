4b768f714ad6bf1d4101437db9d90d6244c979fa
jintak0401 <jintak0401@naver.com>
2022-12-11 16:27:10 +0900

    Revert "feat: ErrorBoundary를 이용해 이미지가 불려오지 않았을 때 fallbackImage 불러오기"
    This reverts commit e841f51867f68c8287f69b9d4326c2dac5193bc6.

--- components/mdxComponents/CustomImage.tsx
+++ components/mdxComponents/CustomImage.tsx
@@ -1,9 +1,5 @@
-import NextImage from 'next/image';
 import React from 'react';
 
-import siteMetaData from '@/data/siteMetadata';
-
-import ErrorBoundary from '@/components/common/ErrorBoundary';
 import Zoom from '@/components/Image/Zoom';
 
 interface Props {
@@ -11,31 +7,19 @@ interface Props {
   alt?: string;
 }
 
-const FallbackImage = ({ alt }: { alt: string }) => (
-  <NextImage
-    className="mx-auto w-auto max-w-full"
-    src={siteMetaData.fallbackImage}
-    alt={alt}
-    width="1000"
-    height="1000"
-  />
-);
-
 const CustomImage = ({ src, alt }: Props) => {
   return (
     <div className="mb-6 space-y-2 md:space-y-3">
-      <ErrorBoundary fallback={<FallbackImage alt={alt as string} />}>
-        <Zoom
-          className="mx-auto w-auto max-w-full"
-          loading="lazy"
-          placeholder="blur"
-          blurDataURL={`/_next/image?url=${src}&w=16&q=1`}
-          src={src as string}
-          alt={alt as string}
-          width="1000"
-          height="1000"
-        />
-      </ErrorBoundary>
+      <Zoom
+        className="mx-auto w-auto max-w-full"
+        loading="lazy"
+        placeholder="blur"
+        blurDataURL={`/_next/image?url=${src}&w=16&q=1`}
+        src={src as string}
+        alt={alt as string}
+        width="1000"
+        height="1000"
+      />
       <div className="middle-text text-center italic">{alt}</div>
     </div>
   );
