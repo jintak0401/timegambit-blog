0ba64c6b15eba809ec38aad7dcbe38f576902cc4
jintak0401 <jintak0401@naver.com>
2022-12-13 22:53:30 +0900

    feat: dynamic import에서 그냥 import로 변경
    react-icons의 BootStrap 아이콘에 버그가 있는 것 같습니다. bs 아이콘들 로드시 모든 아이콘이 import 됩니다.

--- components/mdxComponents/MDXComponents.tsx
+++ components/mdxComponents/MDXComponents.tsx
@@ -1,6 +1,5 @@
 import type { Blog } from 'contentlayer/generated';
 import { ComponentMap } from 'mdx-bundler/client';
-import dynamic from 'next/dynamic';
 import { useMDXComponent } from 'next-contentlayer/hooks';
 import React, { ReactNode } from 'react';
 
@@ -8,11 +7,10 @@ import { coreContent } from '@/lib/contentlayer';
 
 import PostLayout from '@/layouts/PostLayout';
 
-const Alert = dynamic(() => import('./Alert'));
-const CustomImage = dynamic(() => import('./CustomImage'));
-const CustomLink = dynamic(() => import('./CustomLink'));
-const Pre = dynamic(() => import('./Pre'));
-const BlogNewsletterForm = dynamic(() => import('./NewsletterForm'));
+import Alert from './Alert';
+import CustomImage from './CustomImage';
+import CustomLink from './CustomLink';
+import Pre from './Pre';
 
 interface MDXLayout {
   layout: string;
@@ -34,7 +32,6 @@ export const MDXComponents: ComponentMap = {
   a: CustomLink,
   pre: Pre,
   Alert,
-  BlogNewsletterForm,
 };
 
 export const MDXLayoutRenderer = ({ layout, content, ...rest }: MDXLayout) => {
