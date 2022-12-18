147bd55e7234dccbe79515678a26936723ec6b9b
jintak0401 <jintak0401@naver.com>
2022-12-18 10:49:44 +0900

    feat: 바뀐 폴더구조 반영

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,11 @@ module.exports = {
     optimizeUniversalDefaults: true,
   },
   content: [
-    './pages/**/*.tsx',
-    './components/**/*.tsx',
-    './layouts/**/*.tsx',
-    './lib/**/*.ts',
-    './data/**/*.mdx',
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/layouts/**/*.tsx',
+    './src/lib/**/*.ts',
+    './src/data/**/*.mdx',
   ],
   darkMode: 'class',
   theme: {
