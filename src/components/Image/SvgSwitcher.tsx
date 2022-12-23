bedfd9b9c61d40616c077b24fd287cd5e370054b
jintak0401 <jintak0401@naver.com>
2022-12-23 13:29:10 +0900

    feat: svgkey 유무에 따라 LightModeSvg, DarkModeSvg에 넘겨주는 svgkey 다르게 설정

--- src/components/Image/SvgSwitcher.tsx
+++ src/components/Image/SvgSwitcher.tsx
@@ -1,7 +1,10 @@
 import { ReactElement } from 'react';
 
 interface Props {
-  LightModeSvg: (props: { className?: string; svgkey: string }) => ReactElement;
+  LightModeSvg: (props: {
+    className?: string;
+    svgkey?: string;
+  }) => ReactElement;
   DarkModeSvg: (props: { className?: string; svgkey: string }) => ReactElement;
   svgkey?: string;
   className?: string;
@@ -17,12 +20,12 @@ const SvgSwitcher = ({
   return (
     <div className={`relative ${className}`} {...rest}>
       <LightModeSvg
-        svgkey={svgkey}
-        className="absolute h-full w-full opacity-100 duration-500 dark:opacity-0"
+        svgkey={svgkey ? `light_${svgkey}` : ''}
+        className="absolute top-0 h-full w-full opacity-100 duration-500 dark:opacity-0"
       />
       <DarkModeSvg
-        svgkey={svgkey}
-        className="absolute h-full w-full opacity-0 duration-500 dark:opacity-100"
+        svgkey={svgkey ? `dark_${svgkey}` : ''}
+        className="absolute top-0 h-full w-full opacity-0 duration-500 dark:opacity-100"
       />
     </div>
   );
