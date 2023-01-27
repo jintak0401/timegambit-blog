2791d9f6d48a5f17fc98d05b579e4762da05bd93
jintak0401 <jintak0401@naver.com>
2023-01-27 10:29:57 +0900

    feat: ViewCounter를 조회수 증가만 하는 컴포넌트로 변경

--- src/components/blog/ViewCounter.tsx
+++ src/components/blog/ViewCounter.tsx
@@ -1,13 +1,10 @@
 import siteMetadata from 'data/siteMetadata.mjs';
 import { useEffect } from 'react';
-import { IoEyeOutline } from 'react-icons/io5';
 
 import usePostViews from '@/hooks/usePostViews';
 
 interface Props {
   slug: string;
-  shown?: boolean;
-  type?: 'POST' | 'GET';
 }
 
 const LAST_POST = 'last-post';
@@ -15,12 +12,10 @@ const LAST_POST = 'last-post';
 const LIMIT_TIME =
   (siteMetadata.blogPost.viewCountTimeLimit || 0) * 60 * 60 * 1000;
 
-const ViewCounter = ({ slug, type = 'POST', shown = false }: Props) => {
-  const { viewCount, isLoading, isError, increment } = usePostViews(slug);
+const ViewCounter = ({ slug }: Props) => {
+  const { increment } = usePostViews(slug);
 
   useEffect(() => {
-    if (type === 'GET') return;
-
     const { slug: storageSlug, date: storageDate } = JSON.parse(
       localStorage.getItem(LAST_POST) || '{}'
     );
@@ -35,23 +30,10 @@ const ViewCounter = ({ slug, type = 'POST', shown = false }: Props) => {
     localStorage.setItem(LAST_POST, JSON.stringify({ slug, date: new Date() }));
 
     increment();
-  }, [increment, slug, type]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [slug]);
 
-  // when no need to show & fetching is error
-  if (!shown || isError) return null;
-  // loading
-  else if (isLoading)
-    return (
-      <div className="h-5 w-12 animate-pulse rounded-md bg-gray-200 dark:bg-gray-700" />
-    );
-  // success for fetching
-  else
-    return (
-      <div className="flex items-center gap-1.5">
-        <IoEyeOutline size="1.2em" />
-        {viewCount}
-      </div>
-    );
+  return null;
 };
 
 export default ViewCounter;
