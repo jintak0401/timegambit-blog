e613e0de54fdbd94516210c593a40dc95dd1c233
jintak0401 <jintak0401@naver.com>
2023-04-06 16:09:02 +0900

    feat: next.config.js로 redirect 설정 (#414)

--- next.config.js
+++ next.config.js
@@ -3,11 +3,26 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const REDIRECT = {
+  '/blog/blog-log/dynamic-import-og-error':
+    '/blog/solve/dynamic-import-og-error',
+  '/blog/blog-log/contribute-next-auth': '/blog/solve/contribute-next-auth',
+  '/blog/blog-log/svg-usage-fix-bug': '/blog/solve/svg-usage-fix-bug',
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = withContentlayer(
   withBundleAnalyzer({
     reactStrictMode: true,
     swcMinify: true,
+
+    async redirects() {
+      return Object.entries(REDIRECT).map(([source, destination]) => ({
+        source,
+        destination,
+        permanent: false,
+      }));
+    },
 
     images: {
       domains: [
