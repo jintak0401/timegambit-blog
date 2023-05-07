e6a375345d1c0f6b33c09c71c4dfd61ecafe6839
jintak0401 <jintak0401@naver.com>
2023-05-07 20:06:15 +0900

    feat: 스크롤 복원을 위한 커스텀 Link 컴포넌트 구현 (#447)

---
+++ src/components/common/nav-link.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { ComponentProps, forwardRef, MouseEvent } from 'react';
+import NextLink from 'next/link';
+
+import { onStart } from '@/lib/router-events';
+
+const isModifiedEvent = (event: MouseEvent): boolean => {
+  const eventTarget = event.currentTarget as HTMLAnchorElement | SVGAElement;
+  const target = eventTarget.getAttribute('target');
+  return (
+    (target && target !== '_self') ||
+    event.metaKey ||
+    event.ctrlKey ||
+    event.shiftKey ||
+    event.altKey || // triggers resource download
+    (event.nativeEvent && event.nativeEvent.which === 2)
+  );
+};
+
+const NavLink = forwardRef<HTMLAnchorElement, ComponentProps<'a'>>(
+  function Link({ href, onClick, ...rest }, ref) {
+    const useLink = href && href.startsWith('/');
+    if (!useLink) return <a href={href} onClick={onClick} {...rest} />;
+
+    return (
+      <NextLink
+        href={href}
+        onClick={(event) => {
+          if (!isModifiedEvent(event)) {
+            const { pathname, search, hash } = window.location;
+            const curPath = pathname + search + hash;
+            if (href !== pathname + search + hash) onStart(curPath, href);
+          }
+          if (onClick) onClick(event);
+        }}
+        {...rest}
+        ref={ref}
+      />
+    );
+  }
+);
+
+export default NavLink;
