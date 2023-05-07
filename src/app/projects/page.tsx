c647c1ec0720c13bbeed820843d8ac91326ccf88
jintak0401 <jintak0401@naver.com>
2023-05-07 23:02:21 +0900

    feat: /projects 페이지 appDir로 구현 (#447)

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,40 @@
+import { Metadata } from 'next';
+
+import phrases from 'data/phrases';
+import projectData from 'data/projectData';
+import siteMetadata from 'data/siteMetadata.mjs';
+
+import ProjectCard from '@/components/card-and-list/ProjectCard';
+
+export const metadata: Metadata = {
+  title: 'Projects',
+  description: phrases.Seo.projectDesc || siteMetadata.description,
+};
+
+const ProjectPage = () => {
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          {phrases.Project.title}
+        </h1>
+        <div className="middle-text text-lg leading-7">
+          {phrases.Project.description}
+        </div>
+      </div>
+      <div className="-m-4 flex flex-wrap py-12">
+        {projectData.map((d) => (
+          <ProjectCard
+            key={d.title}
+            title={d.title}
+            description={d.description}
+            imgSrc={d.imgSrc}
+            href={d.href}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectPage;
