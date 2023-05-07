1d519dee72d3a170af040f146c7547cf5095a528
jintak0401 <jintak0401@naver.com>
2023-05-08 01:32:35 +0900

    feat: guestbook api 구현 (#447)

---
+++ src/app/api/guestbook/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { RowDataPacket } from 'mysql2';
+import { getServerSession } from 'next-auth';
+
+import { db } from '@/lib/db';
+import { UserType } from '@/lib/types';
+
+import queries from '@/app/api/queries';
+
+export const GET = async () => {
+  let connection = null;
+  try {
+    connection = await db.getConnection();
+    const [guestbooks] = await connection.query<RowDataPacket[]>(
+      queries.READ_ALL_GUESTBOOK
+    );
+    return NextResponse.json(guestbooks);
+  } catch (e) {
+    return NextResponse.json(
+      { message: (e as Error).message },
+      { status: 500 }
+    );
+  } finally {
+    connection && connection.release();
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  let connection = null;
+  try {
+    const session = await getServerSession();
+    const { body } = await req.json();
+
+    if (!session) {
+      return NextResponse.json('Unauthorized', { status: 403 });
+    } else if (!body) {
+      return NextResponse.json('Body is required', { status: 400 });
+    }
+
+    const { email, name, image } = session.user as UserType;
+
+    connection = await db.getConnection();
+    await connection.beginTransaction();
+
+    await connection.query<RowDataPacket[]>(queries.CREATE_GUESTBOOK, [
+      email,
+      name,
+      image,
+      body,
+    ]);
+    await connection.commit();
+
+    return NextResponse.json('success');
+  } catch (e) {
+    connection && (await connection.rollback());
+    return NextResponse.json(
+      { message: (e as Error).message },
+      { status: 500 }
+    );
+  } finally {
+    connection && connection.release();
+  }
+};
