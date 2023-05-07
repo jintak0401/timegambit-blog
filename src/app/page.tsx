40aa73570631f8de64df151219b77e98655fd770
jintak0401 <jintak0401@naver.com>
2023-05-08 02:40:31 +0900

    feat: / 페이지 구현 (#447)

---
+++ src/app/page.tsx
@@ -0,0 +1,77 @@
+import phrases from 'data/phrases';
+import siteMetadata from 'data/siteMetadata.mjs';
+
+import { allBlogs, Blog } from 'contentlayer/generated';
+
+import { pickBlogItem, sortedBlogPost } from '@/lib/contentlayer';
+import { PopularPostType } from '@/lib/types';
+import { PostListItem } from '@/lib/types';
+
+import PostList from '@/components/card-and-list/PostList';
+import Introduction from '@/components/common/Introduction';
+import NavLink from '@/components/common/nav-link';
+
+const getPosts = async () => {
+  const recentPosts = sortedBlogPost(allBlogs)
+    .slice(0, siteMetadata.blogPost.homeRecentPostLength)
+    .map(pickBlogItem) as PostListItem[];
+
+  const res = await fetch(`${process.env.API_URL}/api/views`, {
+    method: 'GET',
+    next: {
+      revalidate: siteMetadata.rebuildPeriod,
+    },
+  });
+
+  let popularPosts: PostListItem[] = [];
+  if (res.ok) {
+    const popularPostsSlug: PopularPostType[] = await res.json();
+    if (popularPostsSlug && popularPostsSlug.length > 0) {
+      popularPosts = popularPostsSlug.map(({ slug }) => {
+        const post = allBlogs.find((item) => item.slug === slug) as Blog;
+        return pickBlogItem(post);
+      }) as PostListItem[];
+    }
+  }
+
+  return {
+    recentPosts,
+    popularPosts,
+  };
+};
+
+const HomePage = async () => {
+  const { recentPosts, popularPosts } = await getPosts();
+
+  return (
+    <div className="flex flex-1 flex-col">
+      <Introduction />
+      <div className="mb-2 flex justify-between xl:mt-2">
+        <h1 className="strong-text text-2xl font-semibold xl:text-3xl">
+          {phrases.Main.blogPosts}
+        </h1>
+        <NavLink
+          href="/blog"
+          className="rounded-md p-2 text-center font-semibold text-primary-500 hover:bg-gray-100 dark:text-primary-400 dark:hover:bg-gray-800 xl:text-lg"
+        >
+          {phrases.Main.allPosts}
+        </NavLink>
+      </div>
+      <h2 className="strong-text mt-2 text-xl font-semibold xl:mt-4 xl:text-2xl">
+        {phrases.Main.recentPosts}
+      </h2>
+      <PostList posts={recentPosts} />
+      {popularPosts && (
+        <>
+          <hr />
+          <h2 className="strong-text mt-4 text-xl font-semibold xl:text-2xl">
+            {phrases.Main.popularPosts}
+          </h2>
+          <PostList posts={popularPosts} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
