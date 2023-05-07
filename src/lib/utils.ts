8c4f675f52fab047ba3b1fbf052e89ae2a17d863
jintak0401 <jintak0401@naver.com>
2023-05-08 01:14:07 +0900

    feat: joinSlugs 유틸함수 구현 (#447)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,22 @@
 import siteMetadata from 'data/siteMetadata.mjs';
 
-const getImageWithFallback = (
+export const getImageWithFallback = (
   url: string,
   fallbackUrl = `${siteMetadata.siteUrl}${url}`
 ) => {
   return url.startsWith('http') ? url : fallbackUrl;
 };
 
-const sameAllElements = (a: unknown[], b: unknown[]) => {
+export const sameAllElements = (a: unknown[], b: unknown[]) => {
   if (a.length !== b.length) return false;
   return a.every((item, idx) => item === b[idx]);
 };
 
-const isTouchDevice = () => {
+export const isTouchDevice = () => {
   if (typeof window === 'undefined') return false;
   return 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 };
 
-export { getImageWithFallback, isTouchDevice, sameAllElements };
+export const joinSlugs = (slug: string | string[] = []) => {
+  return Array.isArray(slug) ? slug.join('/') : slug;
+};
