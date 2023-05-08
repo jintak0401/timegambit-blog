0ba2fbe69b4c036ba417335db2ecf5dbd127489d
jintak0401 <jintak0401@naver.com>
2023-05-08 12:48:18 +0900

    feat: /tags/[tag] 페이지 구현 (#447)

---
+++ src/app/tags/[tag]/page.tsx
@@ -0,0 +1,59 @@
+import { Metadata } from 'next';
+
+import phrases from 'data/phrases';
+import siteMetadata from 'data/siteMetadata.mjs';
+
+import { allBlogs } from 'contentlayer/generated';
+import { slug } from 'github-slugger';
+
+import { pickBlogItem, sortedBlogPost } from '@/lib/contentlayer';
+import { getAllTags } from '@/lib/getBlogInfo.mjs';
+import { PostListItem } from '@/lib/types';
+
+import ListLayout from '@/layouts/ListLayout';
+
+interface Props {
+  params: {
+    tag: string;
+  };
+}
+
+export const dynamicParams = false;
+export const generateStaticParams = async () => {
+  const tags = await getAllTags();
+
+  return Object.keys(tags).map((tag) => ({
+    tag: slug(tag),
+  }));
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  const tag = decodeURI(params.tag);
+  return {
+    title: `Tag - ${tag}`,
+    description:
+      phrases.Seo.specificTagDesc?.replace('%s', tag) ||
+      `#${tag} tags - ${siteMetadata.author}`,
+    alternates: {
+      canonical: `${siteMetadata.siteUrl}/tags/${params.tag}`,
+    },
+  };
+};
+
+const getPosts = (tagSlug: string) => {
+  return sortedBlogPost(allBlogs)
+    .filter(
+      ({ tags, draft }) =>
+        !draft && (tags ?? []).map((t) => slug(t)).includes(tagSlug)
+    )
+    .map(pickBlogItem) as PostListItem[];
+};
+
+const TagPostListPage = async ({ params }: Props) => {
+  console.log(await getAllTags());
+  const tag = decodeURI(params.tag);
+  const posts = getPosts(tag);
+  return <ListLayout posts={posts} title={tag} />;
+};
+
+export default TagPostListPage;
