4eb7056cc8d36b79484ca9bd031033c17df23e15
jintak0401 <jintak0401@naver.com>
2023-06-03 16:39:26 +0900

    refactor: Link->NextLink, 파일이름 tag-in-post로 변경 (#447)

--- src/components/blog/tag-in-post.tsx
+++ src/components/blog/tag-in-post.tsx
@@ -1,5 +1,6 @@
+import NextLink from 'next/link';
+
 import { slug } from 'github-slugger';
-import Link from 'next/link';
 import { AiFillTag } from 'react-icons/ai';
 
 interface Props {
@@ -8,13 +9,13 @@ interface Props {
 
 const TagInPost = ({ title }: Props) => {
   return (
-    <Link
+    <NextLink
       href={`/tags/${slug(title)}`}
-      className="group flex items-center gap-1.5 rounded-md bg-gray-200 py-1 px-2 text-sm text-gray-600 duration-200 hover:bg-gray-600 hover:text-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
+      className="group flex items-center gap-1.5 rounded-md bg-gray-200 px-2 py-1 text-sm text-gray-600 duration-200 hover:bg-gray-600 hover:text-gray-200 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700"
     >
       <AiFillTag className="h-[14px] w-[14px] group-hover:text-gray-200 dark:text-gray-200" />
       {title}
-    </Link>
+    </NextLink>
   );
 };
 
