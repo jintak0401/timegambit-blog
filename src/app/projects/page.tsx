f3d3e43d1ade7fed6653e7e077a288291e4b7aa7
jintak0401 <jintak0401@naver.com>
2023-06-03 23:17:16 +0900

    feat: /projects 페이지에 메타태그 적용 (#447)

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,14 +4,34 @@ import phrases from 'data/phrases';
 import projectData from 'data/project-data';
 import siteMetadata from 'data/site-metadata.mjs';
 
+import { defaultOpenGraph, defaultTwitter } from '@/lib/metadata';
+
 import ProjectCard from '@/components/card-and-list/project-card';
 
-export const metadata: Metadata = {
-  title: 'Projects',
-  description: phrases.Seo.projectDesc || siteMetadata.description,
-  alternates: {
-    canonical: `${siteMetadata.siteUrl}/projects`,
-  },
+export const generateMetadata = (): Metadata => {
+  const title = 'Projects';
+  const ogTitle = siteMetadata.titleTemplate.replace('%s', title);
+  const description = phrases.Seo.projectDesc || siteMetadata.description;
+  const url = `${siteMetadata.siteUrl}/projects`;
+
+  return {
+    title,
+    description,
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      ...defaultOpenGraph,
+      title: ogTitle,
+      description,
+      url,
+    },
+    twitter: {
+      ...defaultTwitter,
+      title: ogTitle,
+      description,
+    },
+  };
 };
 
 const ProjectPage = () => {
