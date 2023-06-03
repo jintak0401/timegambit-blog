b39dc666c2fd18a77bc4101ead167b2b1bab715c
jintak0401 <jintak0401@naver.com>
2023-06-03 17:49:13 +0900

    feat: post-card로 이름 변경 및 client 컴포넌트화 (#447)

--- src/components/card-and-list/post-card.tsx
+++ src/components/card-and-list/post-card.tsx
@@ -1,13 +1,17 @@
-import Link from 'next/link';
+'use client';
+
 import { useState } from 'react';
+
 import { IoEyeOutline, IoHeartOutline } from 'react-icons/io5';
 
 import formattedDate from '@/lib/formattedDate';
-import { PostListItem } from '@/lib/types';
-import { usePostLikes } from '@/hooks/usePostLikes';
+import { usePostLikes } from '@/hooks/use-post-likes';
 import usePostViews from '@/hooks/usePostViews';
 
-import ImageWithFallback from '@/components/Image/ImageWithFallback';
+import NavLink from '@/components/common/nav-link';
+import ImageWithFallback from '@/components/image/image-with-fallback';
+
+import { PostListItem } from '@/types';
 
 import Tag from './Tag';
 
@@ -67,7 +71,7 @@ const PostCard = ({ post }: Props) => {
 
   return (
     <div className="strong-text box-border max-w-full gap-4 space-y-2 rounded-md p-3 duration-300 hover:bg-gray-200 dark:hover:bg-gray-800 md:grid md:grid-cols-5 md:items-center md:space-y-0 xl:grid-cols-3">
-      <Link
+      <NavLink
         href={`/blog/${slug}`}
         className="relative md:col-span-2 xl:col-span-1"
       >
@@ -79,18 +83,18 @@ const PostCard = ({ post }: Props) => {
           onLoadingComplete={() => setLoading(false)}
         />
         <div
-          className={`placeholder absolute top-1/2 left-0 my-auto aspect-[191/100] w-full -translate-y-1/2 overflow-hidden rounded-md bg-gray-200 dark:bg-gray-700 ${
+          className={`placeholder absolute left-0 top-1/2 my-auto aspect-[191/101] w-full -translate-y-1/2 overflow-hidden rounded-md bg-gray-200 dark:bg-gray-700 ${
             loading ? 'block' : 'hidden'
           }`}
         />
-      </Link>
+      </NavLink>
       <div className="flex flex-col justify-between gap-4 md:col-span-3 md:h-full xl:col-span-2">
         <div className="space-y-2 md:space-y-1">
-          <Link href={`/blog/${slug}`}>
+          <NavLink href={`/blog/${slug}`}>
             <h2 className="w-fit cursor-pointer text-start text-xl font-bold leading-8 tracking-tight hover:text-primary-500 xl:text-2xl">
               {title}
             </h2>
-          </Link>
+          </NavLink>
           <ul className="flex flex-wrap">
             {tags &&
               tags.map((tag) => (
