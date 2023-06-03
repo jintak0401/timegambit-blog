4e91e9e88aef6b921157a16150c6a58948fc97b8
jintak0401 <jintak0401@naver.com>
2023-06-03 17:49:27 +0900

    refactor: page-title로 이름 변경 (#447)

--- src/components/blog/page-title.tsx
+++ src/components/blog/page-title.tsx
@@ -1,6 +1,6 @@
-import { PropsWithChildren } from 'react';
+import { ReactNode } from 'react';
 
-export default function PageTitle({ children }: PropsWithChildren) {
+export default function PageTitle({ children }: { children: ReactNode }) {
   return (
     <h1 className="strong-text text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       {children}
