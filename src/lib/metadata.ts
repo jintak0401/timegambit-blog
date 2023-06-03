d764dfe60ce054f6e6b105cb587e44fc282156fc
jintak0401 <jintak0401@naver.com>
2023-06-03 23:29:08 +0900

    feat: 기본 메타태그를 생성하는 함수 구현 (#447)

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,13 @@
 import phrases from 'data/phrases';
 import siteMetadata from 'data/site-metadata.mjs';
 
+interface GenerateDefaultMetadataArgs {
+  title: string;
+  description: string;
+  url: string;
+  images?: string;
+}
+
 export const defaultOpenGraph = {
   url: siteMetadata.siteUrl,
   type: 'website',
@@ -24,4 +31,35 @@ export const defaultTwitter = {
     height: 630,
   },
   site: siteMetadata.twitter,
+};
+
+export const generateDefaultMetadata = ({
+  title,
+  description,
+  url,
+  images,
+}: GenerateDefaultMetadataArgs) => {
+  const ogTitle = siteMetadata.titleTemplate.replace('%s', title);
+  const openGraph = {
+    ...defaultOpenGraph,
+    images: images ?? defaultOpenGraph.images,
+    title: ogTitle,
+    description,
+    url,
+  };
+  const twitter = {
+    ...defaultTwitter,
+    title: ogTitle,
+    description,
+    images: images ?? defaultTwitter.images,
+  };
+  return {
+    title,
+    description,
+    alternates: {
+      canonical: url,
+    },
+    openGraph,
+    twitter,
+  };
 };
