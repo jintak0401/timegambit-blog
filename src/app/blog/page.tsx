154800616057104bc53db42c6091be81a45d3a14
jintak0401 <jintak0401@naver.com>
2023-06-03 23:13:51 +0900

    feat: 블로그 포스트 리스트 페이지에 메타태그 적용 (#447)

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,16 +6,35 @@ import siteMetadata from 'data/site-metadata.mjs';
 import { allBlogs } from 'contentlayer/generated';
 
 import { pickBlogItem, sortedBlogPost } from '@/lib/contentlayer';
+import { defaultOpenGraph, defaultTwitter } from '@/lib/metadata';
 
 import ListLayout from '@/layouts/list-layout';
 
 export const dynamic = 'force-static';
-export const metadata: Metadata = {
-  title: 'Blog',
-  description: phrases.Seo.blogDesc || siteMetadata.description,
-  alternates: {
-    canonical: `${siteMetadata.siteUrl}/blog`,
-  },
+export const generateMetadata = (): Metadata => {
+  const title = 'Blog';
+  const ogTitle = siteMetadata.titleTemplate.replace('%s', title);
+  const description = phrases.Seo.blogDesc || siteMetadata.description;
+  const url = `${siteMetadata.siteUrl}/blog`;
+
+  return {
+    title,
+    description,
+    alternates: {
+      canonical: url,
+    },
+    openGraph: {
+      ...defaultOpenGraph,
+      title: ogTitle,
+      description,
+      url,
+    },
+    twitter: {
+      ...defaultTwitter,
+      title: ogTitle,
+      description,
+    },
+  };
 };
 
 const getPosts = async () => {
