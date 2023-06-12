bf9d51a33811c7228c5fcaba8b8a7cabe7a2aa6d
jintak0401 <jintak0401@naver.com>
2023-06-12 10:46:39 +0900

    feat: 본문에 min-h-screen 추가

--- src/app/layout.client.tsx
+++ src/app/layout.client.tsx
@@ -7,8 +7,6 @@ import siteMetadata from 'data/site-metadata.mjs';
 import { ThemeProvider } from '@wits/next-themes';
 import { ToastContainer } from 'react-toastify';
 
-import { useRestoreScroll } from '@/hooks/use-restore-scroll';
-
 import Footer from '@/components/common/footer';
 import Header from '@/components/common/header';
 
@@ -21,7 +19,7 @@ const BodyLayout = ({ children, className }: Props) => {
   return (
     <>
       <div className="fixed left-0 top-0 -z-10 h-screen w-screen bg-white transition-colors duration-300 dark:bg-gray-900" />
-      <div className="w-section flex flex-col justify-between">
+      <div className="w-section flex min-h-screen flex-col justify-between">
         <Header />
         <main className={`mb-auto mt-24 ${className}`}>{children}</main>
         <hr />
@@ -32,7 +30,7 @@ const BodyLayout = ({ children, className }: Props) => {
 };
 
 const RootLayoutClient = ({ children, className }: Props) => {
-  useRestoreScroll();
+  // useRestoreScroll();
 
   return (
     <>
